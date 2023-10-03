bba97151f4bf62b3c5343a497acfbb10622411fa
yogajones <95644662+yogajones@users.noreply.github.com>
2023-10-03 17:31:42 +0300

    Add schema.sql

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT
+);
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    user_id INTEGER REFERENCES users,
+    sent_at TIMESTAMP
+);

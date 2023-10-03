56e609fbc18d984578ecc9a19415b81fa2dd004d
yogajones <95644662+yogajones@users.noreply.github.com>
2023-10-03 17:30:54 +0300

    Add entry handler

---
+++ messages.py
@@ -0,0 +1,17 @@
+from db import db
+from sqlalchemy import text
+import users
+
+def get_list():
+    sql = "SELECT M.content, U.username, M.sent_at FROM messages M, users U WHERE M.user_id=U.id ORDER BY M.id"
+    result = db.session.execute(text(sql))
+    return result.fetchall()
+
+def send(content):
+    user_id = users.user_id()
+    if user_id == 0:
+        return False
+    sql = "INSERT INTO messages (content, user_id, sent_at) VALUES (:content, :user_id, NOW())"
+    db.session.execute(text(sql), {"content":content, "user_id":user_id})
+    db.session.commit()
+    return True

2fb5d6a44cd0ca3bca233ecc73d992cc73f33c89
yogajones <95644662+yogajones@users.noreply.github.com>
2023-10-05 14:17:38 +0300

    Add learning_journeys table, link tables

--- schema.sql
+++ schema.sql
@@ -3,9 +3,15 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password TEXT
 );
+CREATE TABLE learning_journeys (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    user_id INTEGER REFERENCES users
+);
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     content TEXT,
     user_id INTEGER REFERENCES users,
+    learning_journey_id INTEGER REFERENCES learning_journeys,
     sent_at TIMESTAMP
 );
